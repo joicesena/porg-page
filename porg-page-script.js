feab0eea64ed0996b49cd1b9a8c4cbe76f20f5d2
joicesena <joice.sena.502@gmail.com>
2019-02-26 10:54:10 -0300

    Melhora alternância entre aberturas estatica e animada

--- porg-page-script.js
+++ porg-page-script.js
@@ -1,13 +1,25 @@
 function mudaAberturaHome() {
+	if(document.getElementById("aberturaHomeAnimada").offsetParent === null) {
+		mudaAberturaHomeParaAnimada();	
+	} else if(document.getElementById("aberturaHomeEstatica").offsetParent === null) {
+		mudaAberturaHomeParaEstatica();
+	}
+}
+
+function mudaAberturaHomeParaAnimada() {
 	document.getElementById("aberturaHomeEstatica").style.display = "none";
 	document.getElementById("aberturaHomeAnimada").style.display = "flex";
 	document.getElementById("body").style.overflow = "hidden";
 	document.getElementById("header").classList.add("fade");
+	document.getElementById("titulo").style.marginTop = "25px";
+	document.getElementById("textoFooter").innerHTML="Caso queira, você pode voltar a ver as informações no formato texto ";
 }
 
 function mudaAberturaHomeParaEstatica() {
 	document.getElementById("aberturaHomeAnimada").style.display = "none";
-	// document.getElementById("aberturaHomeEstatica").style.display = "block";
+	document.getElementById("aberturaHomeEstatica").style.display = "block";
 	document.getElementById("body").style.overflow = "visible";
 	document.getElementById("header").classList.remove("fade");
+	document.getElementById("titulo").style.marginTop = "0px";
+	document.getElementById("textoFooter").innerHTML="Caso queira, você pode ver a animação da abertura ";
 }
