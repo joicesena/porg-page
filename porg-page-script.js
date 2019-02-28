d555fad4dc63f79dcb5138f0fa62a59e29769df1
joicesena <joice.sena.502@gmail.com>
2019-02-28 10:28:28 -0300

    Substitui getElementById por querySelector

--- porg-page-script.js
+++ porg-page-script.js
@@ -1,28 +1,28 @@
 function mudaAberturaHome() {
-	if(document.getElementById("aberturaHomeAnimada").offsetParent === null) {
+	if(document.querySelector("#aberturaHomeAnimada").offsetParent === null) {
 		mudaAberturaHomeParaAnimada();	
-	} else if(document.getElementById("aberturaHomeEstatica").offsetParent === null) {
+	} else if(document.querySelector("#aberturaHomeEstatica").offsetParent === null) {
 		mudaAberturaHomeParaEstatica();
 	}
 }
 
 function mudaAberturaHomeParaAnimada() {
-	document.getElementById("aberturaHomeEstatica").style.display = "none";
-	document.getElementById("aberturaHomeAnimada").style.display = "flex";
-	document.getElementById("body").style.overflow = "hidden";
-	document.getElementById("header").classList.add("fade");
-	document.getElementById("titulo").style.marginTop = "25px";
-	document.getElementById("textoFooter").innerHTML="Caso queira, você pode voltar a ver as informações no formato texto ";
+	document.querySelector("#aberturaHomeEstatica").style.display = "none";
+	document.querySelector("#aberturaHomeAnimada").style.display = "flex";
+	document.querySelector("#body").style.overflow = "hidden";
+	document.querySelector("#header").classList.add("fade");
+	document.querySelector("#titulo").style.marginTop = "25px";
+	document.querySelector("#textoFooter").innerHTML= "Caso queira, você pode voltar a ver as informações no formato texto ";
 	setTimeout(function(){
 		mudaAberturaHome();
 	},50000);
 }
 
 function mudaAberturaHomeParaEstatica() {
-	document.getElementById("aberturaHomeAnimada").style.display = "none";
-	document.getElementById("aberturaHomeEstatica").style.display = "block";
-	document.getElementById("body").style.overflow = "visible";
-	document.getElementById("header").classList.remove("fade");
-	document.getElementById("titulo").style.marginTop = "0px";
-	document.getElementById("textoFooter").innerHTML="Caso queira, você pode ver a animação da abertura ";
+	document.querySelector("#aberturaHomeAnimada").style.display = "none";
+	document.querySelector("#aberturaHomeEstatica").style.display = "block";
+	document.querySelector("#body").style.overflow = "visible";
+	document.querySelector("#header").classList.remove("fade");
+	document.querySelector("#titulo").style.marginTop = "0px";
+	document.querySelector("#textoFooter").innerHTML= "Caso queira, você pode ver a animação da abertura ";
 }
